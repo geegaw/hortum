06de55b1a25c67d347f3d063711fae792c0c1a5c
geegaw <cavaugeois@gmail.com>
2016-01-02 09:51:28 -0800

    gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,30 @@
 
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var concat = require("gulp-concat");
 var eslint = require("gulp-eslint");
 var nodemon = require("gulp-nodemon");
 
 var libDir = "./lib/**/*.js";
 var importDir = "./import/**/*.js";
 
-var DIRS = [
+var SERVER_DIRS = [
     "server.js",
     libDir,
     importDir
 ];
 
+
+var PUBLIC = "./public";
+
+var PUBLIC_JS = PUBLIC + "/js";
+var PUBLIC_JS_COMPONENTS = PUBLIC_JS + "/components";
+
+var PUBLIC_CSS = PUBLIC + "/css";
+
+
 gulp.task("lint", function () {
-    return gulp.src(DIRS)
+    return gulp.src(SERVER_DIRS)
           .pipe(eslint({
             extends: "eslint:recommended",
             ecmaFeatures: {
@@ -46,15 +56,21 @@ gulp.task("lint", function () {
 });
 
 gulp.task("babel", function(){
-    return gulp.src(DIRS)
+    return gulp.src(SERVER_DIRS)
         .pipe(babel({
             presets: ["es2015"]
         }))
         .pipe(gulp.dest("dist"));
 });
 
+gulp.task("concat-components", function() {
+  return gulp.src([PUBLIC_JS_COMPONENTS+"/**/*.min.js", "!"+PUBLIC_JS_COMPONENTS+"/components.min.js"])
+    .pipe(concat("components.min.js"))
+    .pipe(gulp.dest(PUBLIC_JS_COMPONENTS+"/"));
+});
+
 gulp.task("watch", function () {
-  gulp.watch(DIRS,["lint"]);
+  gulp.watch(SERVER_DIRS,["lint"]);
 });
 
 gulp.task("server", function(){
